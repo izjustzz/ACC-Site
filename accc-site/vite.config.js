68a2acacbbf50c4074ecdce3b5ecaf00b276a853
izjustzz <zzzurga@gmail.com>
2026-02-11 17:41:21 -0500

    ./src/

--- accc-site/vite.config.js
+++ accc-site/vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // This is important for custom domains
+  base: '', // Change from '/' to '' (empty string)
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 })
